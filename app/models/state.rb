9d3865d99097e41c4ebf3ec851efc57b659e58ee
Chris Thompson <chris.thompson@walkerart.org>
2013-02-20 16:33:11 -0600

    add BG_COLORS to selector

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,4 +1,13 @@
 class State < ActiveRecord::Base
+  BG_COLORS = {
+    "blue"   => "#00ddf0",
+    "purple" => "#b500f0",
+    "orange" => "#f04200",
+    "yellow" => "#f0dd00",
+    "white"  => "#ffffff",
+    "green"  => "#1bdb00",
+    "pink"   => "#f0003e",
+  }
 
   has_many :slides, dependent: :destroy
 
