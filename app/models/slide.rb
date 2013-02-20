f8c56f44304a13f0cc94a2d2d245359224819327
Chris Thompson <chris.thompson@walkerart.org>
2013-02-20 15:47:12 -0600

    configure file fields

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,8 +1,12 @@
 class Slide < ActiveRecord::Base
-  attr_accessible :background, :belongs_to, :position, :text, :slide_type
+  attr_accessible :background, :background_file, :background_color,
+                  :belongs_to, :position, :text, :slide_type,
+                  :retained_background_file, :remove_background_file, :background_file_url
 
   belongs_to :state
   validates_associated :state
+
+  file_accessor :background_file
 
   def to_s
     "s#{position}e#{state.order}"
---
+++ config/initializers/dragonfly.rb
@@ -0,0 +1,34 @@
+require 'dragonfly'
+require 'dragonfly/rails/images'
+
+module S3
+
+  def self.apply_configuration(app, *args)
+    app.configure_with :rails
+    app.configure_with :imagemagick
+    app.configure do |c|
+      c.datastore = Dragonfly::DataStorage::S3DataStore.new
+      c.datastore.configure do |c|
+        c.access_key_id     = ENV['AWS_ACCESS_KEY_ID']
+        c.secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+      end
+      c.job :thumb do |geometry, format|
+        process :thumb, geometry
+        encode format if format
+      end
+    end
+  end
+
+end
+
+cinema_wall = Dragonfly['cinema-wall']
+cinema_wall.configure_with(S3) do |c|
+  c.datastore.configure do |store|
+    store.bucket_name = 'cinema-wall'
+    store.storage_headers = {
+                'x-amz-acl' => 'public-read',
+                'Cache-Control' => 'max-age=315576000',
+                "x-rails-environment" => Rails.env}
+  end
+end
+cinema_wall.define_macro(ActiveRecord::Base, :file_accessor)
