ff4eff8b29c4f97ae40d4e9036e4d96b7628df28
Chris Thompson <chris.thompson@walkerart.org>
2013-03-12 17:42:51 -0500

    add order to slide

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130311234611) do
+ActiveRecord::Schema.define(:version => 20130312171401) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(:version => 20130311234611) do
     t.integer  "state_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "order"
   end
 
   create_table "states", :force => true do |t|
