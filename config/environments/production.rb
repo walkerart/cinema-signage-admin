b2a65675c44461b6f033d5a81c2d3c73afa7dd02
Chris Thompson <chris.thompson@walkerart.org>
2013-03-14 17:40:38 -0500

    oh just serve the assets for now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,12 +3,11 @@ CinemaWall::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.serve_static_assets = false
+  config.serve_static_assets = true
   config.assets.compress = true
   config.assets.compile = true
   config.assets.initialize_on_precompile = true
   config.assets.digest = true
-  config.assets.precompile += %w( search.js )
 
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
   config.i18n.fallbacks = true
