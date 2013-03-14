5e9c6c2cdfe5c8fcb56807d81b3da2fa2a0dc373
Chris Thompson <chris.thompson@walkerart.org>
2013-03-14 16:12:03 -0500

    add unicorn config

---
+++ config/unicorn.rb
@@ -0,0 +1,69 @@
+# from defunkt: https://gist.github.com/206253
+rails_env = ENV['RAILS_ENV'] || 'production'
+
+pid 'tmp/pids/unicorn.pid'
+
+stderr_path 'log/unicorn.log'
+
+listen File.absolute_path('tmp/sockets/unicorn.sock'), :backlog => 2048
+
+worker_processes 4
+
+# Load rails+project into the master before forking workers
+# for super-fast worker spawn times
+preload_app true
+
+# Restart any workers that haven't responded in 30 seconds
+timeout 30
+
+before_fork do |server, worker|
+  ##
+  # When sent a USR2, Unicorn will suffix its pidfile with .oldbin and
+  # immediately start loading up a new version of itself (loaded with a new
+  # version of our app). When this new Unicorn is completely loaded
+  # it will begin spawning workers. The first worker spawned will check to
+  # see if an .oldbin pidfile exists. If so, this means we've just booted up
+  # a new Unicorn and need to tell the old one that it can now die. To do so
+  # we send it a QUIT.
+  #
+  # Using this method we get 0 downtime deploys.
+
+  old_pid = 'tmp/pids/unicorn.pid.oldbin'
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+
+after_fork do |server, worker|
+  ##
+  # Unicorn master loads the app then forks off workers - because of the way
+  # Unix forking works, we need to make sure we aren't using any of the parent's
+  # sockets, e.g. db connection
+
+  ActiveRecord::Base.establish_connection
+  # CHIMNEY.client.connect_to_server
+  # Redis and Memcached would go here but their connections are established
+  # on demand, so the master never opens a socket
+
+
+  ##
+  # Unicorn master is started as root, which is fine, but let's
+  # drop the workers to deployer:deployer
+  unless Rails.env == 'development'
+    uid, gid = Process.euid, Process.egid
+    user, group = 'deployer', 'deployer'
+    target_uid = Etc.getpwnam(user).uid
+    target_gid = Etc.getgrnam(group).gid
+    worker.tmp.chown(target_uid, target_gid)
+    if uid != target_uid || gid != target_gid
+      Process.initgroups(user, target_gid)
+      Process::GID.change_privilege(target_gid)
+      Process::UID.change_privilege(target_uid)
+    end
+  end
+end
