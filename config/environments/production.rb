48c0c13c32e41a9a3486c3d91b90d2bc8bdd7362
Chris Thompson <chris.thompson@walkerart.org>
2013-03-14 18:16:59 -0500

    don't serve static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ CinemaWall::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.serve_static_assets = true
+  config.serve_static_assets = false
   config.assets.compress = true
   config.assets.compile = true
   config.assets.initialize_on_precompile = true
